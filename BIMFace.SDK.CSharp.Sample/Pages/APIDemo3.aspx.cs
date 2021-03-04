830f528f49b22bf1a5f4696baf66223c7b64451f
Savion <905442693@qq.com>
2021-03-04 21:30:30 +0800

    增加 文件直传接口示例 APIDemo3.aspx

--- BIMFace.SDK.CSharp.Sample/Pages/APIDemo3.aspx.cs
+++ BIMFace.SDK.CSharp.Sample/Pages/APIDemo3.aspx.cs
@@ -8,20 +8,14 @@ using BIMFace.SDK.CSharp.Entity.Response;
 
 namespace BIMFace.SDK.CSharp.Sample.Pages
 {
-    /// <summary>
-    /// 示例2：源文件上传
-    /// </summary>
-    public partial class APIDemo2 : System.Web.UI.Page
+    public partial class APIDemo3 : System.Web.UI.Page
     {
         readonly string _appKey = ConfigUtility.GetAppSettingValue("BIMFACE_AppKey");
         readonly string _appSecret = ConfigUtility.GetAppSettingValue("BIMFACE_AppSecret");
 
         protected void Page_Load(object sender, EventArgs e)
         {
-            if (!IsPostBack)
-            {
-
-            }
+          
         }
 
         // 获取 AccessToken
@@ -35,28 +29,26 @@ namespace BIMFace.SDK.CSharp.Sample.Pages
             }
         }
 
-        // 普通文件流上传
-        protected void btnUploadFileByStream_Click(object sender, EventArgs e)
+        // 获取policy凭证
+        protected void btnGetPolicy_Click(object sender, EventArgs e)
         {
-            string filePath = FileUpload1.PostedFile.FileName;//必须在IE兼容模式下才能获取到文件的绝对路径
+            string filePath = FileUpload1.PostedFile.FileName; //必须在IE兼容模式下才能获取到文件的绝对路径
 
             FileInfo fileInfo = new FileInfo(filePath);
             string fileName = fileInfo.Name;
-            var fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read);
-            
+
             IFileApi api = new FileApi();
-            FileUploadResponse fileUploadResponse = api.UploadFileByStream(txtAccessToken.Text, fileName, fileStream);
-            txtResult.Text = fileUploadResponse.SerializeToJson(true);
+            FileUploadPolicyResponse policyResponse = api.GetFileUploadPolicy(txtAccessToken.Text, fileName);
+            txtResult.Text = policyResponse.SerializeToJson(true);
         }
 
-        // 指定外部文件url方式上传
+        // 文件直传
         protected void btnUploadFileByUrl_Click(object sender, EventArgs e)
         {
-            string fileName = "test.rvt";
-            string fileUrl = "xxxx/test.rvt";// 请替换成自己真实业务场景中的图纸所在的url地址
-
+            string filePath = FileUpload1.PostedFile.FileName; //必须在IE兼容模式下才能获取到文件的绝对路径
+            
             IFileApi api = new FileApi();
-            FileUploadResponse fileUploadResponse = api.UploadFileByUrl(txtAccessToken.Text, fileName, fileUrl);
+            FileUploadResponse fileUploadResponse = api.UploadFileByPolicy(txtAccessToken.Text, filePath);
             txtResult.Text = fileUploadResponse.SerializeToJson(true);
         }
 
